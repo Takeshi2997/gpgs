8f4fe1382535f2490cdb06b384513b6c790b6ca4
Takeshi2997 <takeshi6.6260@gmail.com>
2021-05-16 15:25:22 +0900

    fix

--- setup.jl
+++ setup.jl
@@ -1,7 +1,7 @@
 module Const
 
 # System Size
-const dim = 20
+const dim = 80
 
 # System Param
 const h = 1f0
@@ -9,7 +9,7 @@ const J = -1f0
 const Δτ = 0.1f0
 
 # Repeat Number
-const init  = 24
+const init  = 128
 const burnintime = 10
 const iters = 100
 const iT    = 200
@@ -17,7 +17,7 @@ const batchsize = 16
 
 # Hyper Param
 const θ₁ = 1f0
-const θ₂ = 40f0
+const θ₂ = 10f0
 const α  = 1f-6
 
 end
